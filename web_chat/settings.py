4da43e95abc456eb9e48379cac4ee3bcae76a6d6
nghiem <nghiem@nghiem.nghiem>
2020-12-16 10:17:56 +0700

    update

--- web_chat/settings.py
+++ web_chat/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = '/home/nghiem/web_chat'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
